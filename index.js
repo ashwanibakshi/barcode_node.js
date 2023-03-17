6cc48d84d5008a448f31187825e1feecb5ed333c
ashwanibakshi <ab324002@gmail.com>
2023-03-17 19:57:06 +0530

    commit

--- index.js
+++ index.js
@@ -6,25 +6,46 @@ const JsBarcode = require('jsbarcode');
 
 const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-// Create a new canvas
-const canvas = new Canvas();
-JsBarcode(canvas,"hello", {
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+// // Create a new canvas
+// const canvas = new Canvas();
+// JsBarcode(canvas,"hello", {
+//   lineColor: "#0aa",
+//   width:4,
+//   height:40,
+//   displayValue: false
+// });
+
+// Get Data URL and save it to the DB
+app.get('/',(req,res)=>{
+     res.render('index',{data:''});
+})
+
+app.post('/',(req,res)=>{
+  console.log('dfd',req.body.qrcodee);
+  const canvas = new Canvas();
+ JsBarcode(canvas,req.body.qrcodee,{
   lineColor: "#0aa",
   width:4,
   height:40,
   displayValue: false
-});
-
-// Get Data URL and save it to the DB
-
-app.get('/',(req,res)=>{
-  canvas.toDataURL('image/png', (err, png) => {
+  });
+     canvas.toDataURL('image/png', (err, png) => {
     console.log(png);
     res.render('index',{data:png});
   })
-});
+})
+
+// app.get('/',(req,res)=>{
+//   canvas.toDataURL('image/png', (err, png) => {
+//     console.log(png);
+//     res.render('index',{data:''});
+//   })
+// });
 
 app.listen(3000,console.log(`server run at 3000`));
 
